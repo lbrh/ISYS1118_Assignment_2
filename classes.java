e49900a1fa74f9f9ded10cf828c22f7d32a15523
lbrh <s4051927@student.rmit.edu.au>
2024-09-22 06:13:46 -0500

    done?

--- classes.java
+++ classes.java
@@ -1,49 +1,120 @@
-abstract class Customer {
+class Customer {
+    String sessionID;
+    Product[] browseHistory;
+
     public void browseProducts() {}
-    public void addToCart(Product product) {}
-    public void viewProductDetails(Product product) {}
-    public void virtualTryOn(Product product) {}
+
+    public void addToCart(int productID, int quantity) {}
+
+    public void getProductDetails(int productID) {}
 }
 
 class GuestCustomer extends Customer {
-    String guestId;
-    
-    public void login() {} // NOTE: Log in with customerId?
-    public void createAccount() {}
+    int guestID;
 }
 
 class RegisteredCustomer extends Customer {
-    String customerId;
-    
-    public void checkout(Cart cart) {}
-    public void bookEyeTest(Optician optician) {}
+    int customerID;
+    String emailAddress;
+
+    public void login() {}
+
+    public void createAccount() {}
+
+    public void viewAppointments() {}
+
+    public void checkout(ShoppingCart cart) {}
+
+    public void virtualTryOn(int productID) {}
 }
 
-class Optician {
-    String opticianId;
-    String schedule;
-}
+class ShoppingCart {
+    Product[] items;
+    double totalPrice;
+    int quantity;
 
-class ExternalAPIService {
-    String serviceName;
-    String apiUrl;
-    
-    public void service(String serviceName) {}
+    public void applyCoupon(String couponCode) {}
+
+    public void clearCart() {}
 }
 
 class Product {
-    String productId;
+    int productID;
     String productName;
     double price;
-    String details;
+    String description;
+    int quantity;
+
+    public void updateStock(int quantity) {}
 }
 
-class Cart {
-    String cartId;
-    Product[] items;
-    double totalPrice;
+class Staff {
+    int staffID;
+    String name;
+    String email;
 
-    public void addToCart(Product product) {}
-    // TODO: remove from cart/update quantity and whatnot? 
-    public void checkout(ExternalAPIService pay) {}
+    public void manageOrder(int orderID, String action) {}
+
+    public void handleReturn(int orderID) {}
+}
+
+class Admin extends Staff {
+    int adminID;
+
+    public void updateInventory(Product[] inventory) {}
+
+    public void addProduct(Product product) {}
+
+    public void deleteProduct(int productID) {}
+
+    public void changePrice(int productID, int price) {}
+}
+
+class Order {
+    int orderID;
+    String orderDate;
+    int customerID;
+    Product[] items;
+
+    public void getOrderDetails() {}
+
+    public void generateInvoice() {}
+}
+
+class Optician {
+    int opticianID;
+    String name;
+    String[] schedule;
+
+    public void addAvailability(String date, String time) {}
+
+    public void getAppointments() {}
+
+    public void createAppointment(int opticianID, String date, String time) {}
+}
+
+class Appointment {
+    int appointmentID;
+    int customerID;
+    int opticianID;
+    String appointmentDate;
+    String appointmentTime;
+}
+
+class Prescription {
+    int prescriptionID;
+    String prescriptionDetails;
+
+    public void printPrescription() {}
+
+    public void linkToOrder(int prescriptionID, int orderID) {}
+}
+
+class ExternalAPIService {
+    String tryOnAPIEndpoint;
+    String paymentAPIEndpoint;
+
+    public void startVirtualTryOn(int productID, Image userImage) {}
+
+    public void processPayment(int orderID, String paymentDetails) {}
 }
